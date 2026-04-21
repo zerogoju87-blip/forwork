79fc9cecc583cb3da91b2893c31dfbfd77f95a44
zerogoju87-blip <zerogoju87@gmail.com>
2026-04-21 06:01:50 -1100

    Rename home route to index in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\PortfolioController;
 
-Route::get('/', [PortfolioController::class, 'index'])->name('home');
+Route::get('/', [PortfolioController::class, 'index'])->name('index');
 Route::get('/projects', [PortfolioController::class, 'projects'])->name('projects');
 Route::get('/contact', [PortfolioController::class, 'contact'])->name('contact');
 Route::post('/contact', [PortfolioController::class, 'send'])->name('contact.send');
