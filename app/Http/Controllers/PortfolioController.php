3bbf43a5fed993ad5c664a23ed2aef2c2984467e
zerogoju87-blip <zerogoju87@gmail.com>
2026-04-21 06:03:09 -1100

    Change view from 'home' to 'index' in index method

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -8,7 +8,7 @@ class PortfolioController extends Controller
 {
     public function index()
     {
-        return view('home');
+        return view('index');
     }
 
     public function projects()
